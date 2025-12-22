c16a89269128c20d05951630e4d25eb58fa61e05
horizoncorpgi-maker <horizoncorpgi@gmail.com>
2025-12-22 15:46:00 -0300

    Updated Home1.tsx

--- src/pages/Home1.tsx
+++ src/pages/Home1.tsx
@@ -337,7 +337,7 @@ function Home1() {
 
   const handleAcceptOffer = () => {
     setShowUpsellPopup(false);
-    window.location.href = appendParamsToUrl('https://pay.erectosbrutallis.com/checkout/197875571:1');
+    window.location.href = appendParamsToUrl('https://erectosbrutallis.co/a/direct/2nsvbL?type=link');
   };
 
   const handleRefuseOffer = () => {
