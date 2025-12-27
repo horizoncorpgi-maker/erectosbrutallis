8e7bd3ad01646411088b56b3fc64492256336be6
horizoncorpgi-maker <horizoncorpgi@gmail.com>
2025-12-27 16:52:13 -0300

    Updated Home1.tsx

--- src/pages/Home1.tsx
+++ src/pages/Home1.tsx
@@ -612,7 +612,7 @@ function Home1() {
                 <button
                   ref={sixBottleButtonRef}
                   id="six-bottle-button"
-                  onClick={() => window.location.href = appendParamsToUrl('https://erectosbrutallis.co/a/direct/2nsvbL?type=link')}
+                  onClick={() => window.location.href = appendParamsToUrl('https://pay.erectosbrutallis.com/checkout/197875571:1')}
                   className="w-full max-w-md mx-auto bg-[#FFD600] text-gray-900 py-3 md:py-6 rounded-full font-bold hover:bg-[#FFC400] transition-all shadow-lg text-base md:text-2xl mb-3 md:mb-6"
                 >
                   CLAIM OFFER NOW
